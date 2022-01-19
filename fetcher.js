a2e45809249f295906583408e0182c6378856e7a
Neale Taylor <nealeptaylor@gmail.com>
2022-01-19 16:51:57 -0500

    added readline and what to do when file already exists

--- fetcher.js
+++ fetcher.js
@@ -1,16 +1,33 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 const args = process.argv.slice(2);
-console.log(args[0]);
+
 request(args[0], (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-  fs.writeFile(args[1], body, err => {
-    if (err) {
-      console.error(err);
-      return;
+  fs.writeFile(args[1], body, { flag: 'wx' }, err => {
+    if (err.code === 'EEXIST') {
+      rl.question("File already exists, would you like to overwrite?(y/n)", (answer) => {
+        if (answer === "y") {
+          fs.writeFile(args[1], body, () => {
+            console.log(`File overwritten!. Downloaded and saved ${body.length} bytes to ${args[1]}`);
+          });
+        } else if (answer === "n") {
+          console.log(`${args[0]} will not be downloaded. Goodbye.`);
+        }
+        rl.close();
+
+      });
+
     }
+    return;
   });
   console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
 });
