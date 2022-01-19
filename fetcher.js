09f056ef377a3819c79ebded053b7819c7f4636c
Neale Taylor <nealeptaylor@gmail.com>
2022-01-19 17:32:14 -0500

    added other error messages

--- fetcher.js
+++ fetcher.js
@@ -9,25 +9,32 @@ const rl = readline.createInterface({
 const args = process.argv.slice(2);
 
 request(args[0], (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
   fs.writeFile(args[1], body, { flag: 'wx' }, err => {
-    if (err.code === 'EEXIST') {
+    if (response.statusCode !== 200) {
+      console.log("URL is invalid. Check command line URL and try again.");
+      rl.close();
+      return;
+    }
+    if (err.code === "ENOENT") {
+      console.log("Local file path is invalid. Check command line file path and try again.");
+      rl.close();
+      return;
+    } else if (err.code === 'EEXIST') {
       rl.question("File already exists, would you like to overwrite?(y/n)", (answer) => {
         if (answer === "y") {
           fs.writeFile(args[1], body, () => {
-            console.log(`File overwritten!. Downloaded and saved ${body.length} bytes to ${args[1]}`);
+            console.log(`File overwritten!. Downloaded and saved ${body.length} bytes to ${args[1]}.`);
+            rl.close();
           });
         } else if (answer === "n") {
-          console.log(`${args[0]} will not be downloaded. Goodbye.`);
+          console.log(`${args[0]} will not be downloaded. Goodbye!`);
+          rl.close();
         }
-        rl.close();
-
       });
-
+      return;
+    } else if (error === null) {
+      console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}.`);
+      rl.close();
     }
-    return;
   });
-  console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
 });
